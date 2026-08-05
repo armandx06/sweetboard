f9b997ee2c07b2d705482ddda179ef871a8c618d
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2026-08-05 11:53:35 -0600

    fix(config): update PostgreSQL host and port settings in config and .env.example

--- apps/api/app/core/config.py
+++ apps/api/app/core/config.py
@@ -5,14 +5,14 @@ class Settings(BaseSettings):
     postgres_user: str
     postgres_password: str
     postgres_db: str
-    postgres_host: str = "localhost"
-    postgres_host_port: int = 5432
+    postgres_host: str = "db"
+    postgres_internal_port: int = 5432
 
     model_config = SettingsConfigDict(env_file="../../.env", extra="ignore")
 
     @property
     def database_url(self) -> str:
-        return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_host_port}/{self.postgres_db}"
+        return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_internal_port}/{self.postgres_db}"
 
 
 settings = Settings()  # type: ignore[call-arg]
